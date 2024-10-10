b5b52e0cbc2f394bb2ff426be7c14ff35856c553
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-10-10 13:17:49 +0400

    commit 2

--- GitTestDZ1/Program.cs
+++ GitTestDZ1/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("GitTestDZ1!");
             Console.WriteLine("GitTestDZ2!");
+            Console.WriteLine("GitTestDZ2! second commit");
         }
     }
 }
